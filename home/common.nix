a6cc1c4f7ba23f5a90b781affe61847c6d5101ca
gio <giordano2407@gmail.com>
2025-05-10 23:14:19 +0200

    feat: new hx keybinds + rg to ignore git files

--- home/common.nix
+++ home/common.nix
@@ -83,13 +83,13 @@
       # ripgrep + fzf
       ff() {
         if [ ! "$#" -gt 0 ]; then echo "Need a string to search for!"; return 1; fi
-        rg --files-with-matches --no-messages --hidden "$1" | fzf --preview "highlight -O ansi -l {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$1' || rg --ignore-case --pretty --context 10 '$1' {}"
+        rg --files-with-matches --no-messages --hidden "$1" --glob "!.git/*" | fzf --preview "highlight -O ansi -l {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$1' || rg --ignore-case --pretty --context 10 '$1' {}"
       }
 
       # ripgrep + fzf -> helix
       fh() {
         if [ ! "$#" -gt 0 ]; then echo "Need a string to search for!"; return 1; fi
-        rg --files-with-matches --no-messages --hidden "$1" | fzf --preview "highlight -O ansi -l {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$1' || rg --ignore-case --pretty --context 10 '$1' {}" | xargs hx
+        rg --files-with-matches --no-messages --hidden "$1" --glob "!.git/*" | fzf --preview "highlight -O ansi -l {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$1' || rg --ignore-case --pretty --context 10 '$1' {}" | xargs hx
       }
 
       # Environment variables
@@ -253,6 +253,7 @@
   # Define user environment packages
   home.packages = with pkgs; [
     htop
+    lsof
 
     google-chrome
     firefox
