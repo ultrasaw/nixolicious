c8b0b0eb93f1436d78f25463ecff0bb785277637
gio <giordano2407@gmail.com>
2026-03-08 17:59:15 +0100

    chore: set wp manually for now

--- modules/greetd_niri.nix
+++ modules/greetd_niri.nix
@@ -17,7 +17,7 @@ let
     systemctl --user start waybar.service &
 
     # wallpaper
-    ${pkgs.swww}/bin/swww img ../assets/000011580013.JPG} &
+    ${pkgs.swww}/bin/swww img ../assets/000011580013.JPG} & # does not work, set manuall w/ swww img /home/gio/Documents/_projects/nixolicious/assets/000011580013.jpg
 
     # idle locking
     systemctl --user start swayidle.service &
