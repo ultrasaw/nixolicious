1fcd861377269894e18dedc7b37a3f185e305d8a
gio <giordano2407@gmail.com>
2026-06-28 19:06:22 +0200

    fix: steam to not use nvidia global cache

--- modules/steam.nix
+++ modules/steam.nix
@@ -3,6 +3,18 @@
 {
   programs.steam = {
     enable = true;
+    package = pkgs.steam.override {
+      extraProfile = ''
+        # Keep globally exported CUDA libraries from leaking into Steam's runtime.
+        unset LD_LIBRARY_PATH
+
+        steam_nvidia_cache_home="''${XDG_CACHE_HOME:-$HOME/.cache}/nvidia"
+        mkdir -p "$steam_nvidia_cache_home/GLCache"
+        export __GL_SHADER_DISK_CACHE=1
+        export __GL_SHADER_DISK_CACHE_PATH="$steam_nvidia_cache_home/GLCache"
+        export __GL_SHADER_DISK_CACHE_SIZE=10737418240
+      '';
+    };
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     gamescopeSession.enable = true;
